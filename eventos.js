85900bc3a9a558e6b2f1fc13b4677e445cb8b746
Matías <matiascas13@gmail.com>
2023-08-11 14:44:23 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const saludarButton = document.getElementById('saludarButton');
     const botonContainer = document.getElementById('botonContainer');
 
-    saludarButton.addEventListener('click', function() {
+    saludarButton.addEventListener('click', function(event) {
+        event.stopPropagation(); // Detiene la propagación del evento
         alert('¡Hola desde el botón!');
     });
 
